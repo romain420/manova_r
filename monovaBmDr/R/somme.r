637bdf605b753d46ee3acbb17b047e2257ae6851
romain420 <romain.dreuilhet@protonmail.com>
2021-12-06 17:54:32 +0100

    package set-up

---
+++ monovaBmDr/R/somme.r
@@ -0,0 +1,14 @@
+#' somme
+#'
+#' @param x a number
+#' @param y a number
+#'
+#' @return a number
+#' @export
+#'
+#' @examples
+#' somme(2,2)
+#' somme(42,69ly)
+somme <- function(x, y){
+  x+y
+}
